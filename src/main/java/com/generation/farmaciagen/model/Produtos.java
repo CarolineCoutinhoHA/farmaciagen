c51efdc699aea525cb6038dbef4152be326405fe
Corolaine <contacaroline.coutinho@gmail.com>
2024-07-22 18:47:05 -0300

    Produtos na branch

---
+++ src/main/java/com/generation/farmaciagen/model/Produtos.java
@@ -0,0 +1,90 @@
+package com.generation.farmaciagen.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produtos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O Atributo nome é Obrigatório!")
+    @Size(min = 5, max = 100, message = "O atributo nome deve conter no mínimo 05 e no máximo 100 caracteres")
+    private String nome;
+
+    @NotBlank(message = "O atributo descrição é Obrigatório!")
+    @Size(min = 10, max = 1000, message = "O atributo descrição deve conter no mínimo 10 e no máximo 1000 caracteres")
+    private String descricao;
+
+    private LocalDate validade;
+
+    @NotNull(message = "O atributo preço é Obrigatório!")
+    @DecimalMin(value = "0.0", inclusive = false, message = "O preço deve ser maior que zero")
+    private Double preco;
+
+    @ManyToOne
+    @JsonIgnoreProperties("produtos")
+    private Categoria categoria;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome (String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao (String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getValidade() {
+        return validade;
+    }
+
+    public void setValidade(LocalDate validade) {
+        this.validade = validade;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
